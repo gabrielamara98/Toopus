0ee8f32a450ac94820f25507d82c66768c33eabd
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-11-16 18:27:45 -0300

    Json Inserido + map no card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,30 @@
-import { FaCss3Alt } from "react-icons/fa";
-import { FaPython } from "react-icons/fa6";
-import { FaHtml5 } from "react-icons/fa";
-
+import dadosPerfil from "../data/usersDetail.json"
 
 function Card(){
     return(
-        <div className="grid auto-rows-auto w-sm border justify-items-center gap-6 px-5 py-5 rounded-md">
-            <div className="w-40 h-40 rounded-full overflow-hidden border-3">
-                <img className="w-full h-full object-cover"src="https://i.pinimg.com/564x/7c/3d/7a/7c3d7a5e2ca5ebafe4346470a9efa667.jpg"></img>
-            </div>
-            <div className="flex flex-col text-center gap-3">
-                <p>Gabriel Amara</p>
-                <p>Desenvolvedor de Software</p>
-            </div>
-            <div className="flex flex-row gap-4 text-lg">
-                <FaCss3Alt/>
-                <FaPython/>
-                <FaHtml5/>
-            </div>
-            <button className="border rounded-full min-w-xs py-2 px-2 ">Ver mais</button>
-        </div>
+        <>
+            {dadosPerfil.map((items) =>{
+                return(
+                    <div key={items.id} className="grid auto-rows-auto w-sm border justify-items-center gap-6 px-5 py-5 rounded-md">
+                        <div className="w-40 h-40 rounded-full overflow-hidden border-3">
+                            <img className="w-full h-full object-cover"src={items.foto}></img>
+                        </div>
+                        <div className="flex flex-col text-center gap-3">
+                            <p>{items.nome}</p>
+                            <p>{items.cargo}</p>
+                        </div>
+                        <div className="flex flex-row gap-4 text-lg">
+                            {items.habilidadesTecnicas.map((skills) =>{
+                                return(
+                                    <p>{skills}</p>
+                                )
+                            })}
+                        </div>
+                        <button className="border rounded-full min-w-xs py-2 px-2 ">Ver mais</button>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 export default Card;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,8 +3,10 @@ import Card from "../components/Card";
 function Search(){
     return(
         <>
-        <h2>Pagina de busca</h2>
-        <Card></Card>
+            <h2>Pagina de busca</h2>
+            <div className="grid grid-cols-3 w-fit place-content-center gap-15 justify-self-center">
+                <Card></Card>
+            </div>
         </>
         
     )
