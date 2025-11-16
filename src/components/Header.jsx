47bb01e04f5412801cbece160c58f5bd27d34e6c
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-11-16 14:58:28 -0300

    basic Header + Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,25 @@
-import { HiAcademicCap } from "react-icons/hi2";
+import { IoHome } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
+import { IoIosInformationCircle } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 function Header(){
     return(
-        <header>
-            <h1>TooPus</h1>
-            <div>
-                <HiAcademicCap />
-                <HiAcademicCap />
-                <HiAcademicCap />
-            </div>
+        <header className="grid grid-cols-2 items-center px-10 py-5 border min-w-full">
+            <h1 className="flex items-center gap-2 text-4xl" >Toopus</h1>
+            <nav>
+                <ul className="flex justify-center gap-10">
+                    <li>
+                        <Link to={"/"} className="flex items-center gap-2 text-lg"><IoHome/>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/search"} className="flex items-center  gap-2 text-lg"><FaSearch/>Search</Link>
+                    </li>
+                    <li>
+                        <Link to={"/about"} className="flex items-center  gap-2 text-lg"><IoIosInformationCircle />About</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
