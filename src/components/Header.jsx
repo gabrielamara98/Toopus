017755b7b93c8ea5a20ad1497fa880fe52f794ee
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-11-16 13:47:55 -0300

    Setup Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { HiAcademicCap } from "react-icons/hi2";
+
+function Header(){
+    return(
+        <header>
+            <h1>TooPus</h1>
+            <div>
+                <HiAcademicCap />
+                <HiAcademicCap />
+                <HiAcademicCap />
+            </div>
+        </header>
+    )
+}
+export default Header;
