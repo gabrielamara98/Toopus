c44f565bd9ffd7c788b30b03f9fff8e01c552bea
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2025-11-18 21:45:30 -0300

    Organization of Search page + Members Tab

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,6 @@
-import Card from "../components/Card";
-
 function Search(){
     return(
         <>
-            <div className="grid grid-cols-3 w-fit place-content-center gap-15 justify-self-center">
-                <Card></Card>
-            </div>
         </>
         
     )
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,16 @@
 import {createBrowserRouter} from 'react-router-dom'
 import RootLayout from '../pages/RootLayout'
 import Search from '../pages/Search'
+import Members from '../pages/Member'
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
 		element : <RootLayout/>,
 		children:[
-			{path:'search', element: <Search/>}
+			{path:'search', element: <Search/>},
+			{path:'members', element: <Members/>},
+
 		]
 	}
 ])
